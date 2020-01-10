eccf1b310390c8379a8bf2abc7abb532187a5dd0
Ruslan Vasiliev <ruzero.io@gmail.com>
2020-01-11 01:00:38 +0200

    gallery

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slick-gallery').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    prevArrow: '#prev-slide',
+    nextArrow: '#next-slide'
+  })
+});
